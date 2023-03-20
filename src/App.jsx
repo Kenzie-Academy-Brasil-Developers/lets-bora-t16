18a704d43f44dac55041337e6d0146ede0948a70
tsunodajapa <gabrieltsunoda@gmail.com>
2023-03-20 11:18:05 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './styles/App.css'
+import './styles/teste.css'
+
+
+function App() {
+  const abacaxi = 'teste'
+
+  console.log('Olá', abacaxi)
+
+  function teste() {
+
+  }
+
+  return (
+    <div className="App">
+      <h1>Teste</h1>
+
+
+      <form>
+        <label htmlFor=''></label>
+        <input type="text" />
+        <input type="text" />
+
+      </form>
+    </div>
+  )
+}
+
+export default App;
+
+// javascript + XML
+// class -> className 
+// for -> htmlFor
+// <img > -> <img />
+
+
